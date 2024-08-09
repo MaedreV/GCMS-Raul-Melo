343fee27e8c263c3524082332047188afc79985a
Raul Melo <raulvitor123456@hotmail.com>
2024-08-09 13:56:15 -0300

    1 commit

---
+++ src/main/java/br/edu/ifpe/recife/gcms/WebApp/HomeController.java
@@ -0,0 +1,17 @@
+package br.edu.ifpe.recife.gcms.WebApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public @ResponseBody String greetingFulano() {
+	return "Hello, fulano!";
+	}
+	@RequestMapping("/Raul")
+	public @ResponseBody String greetingRaul() {
+	return "Hello, Raul!";
+	}
+
+}
